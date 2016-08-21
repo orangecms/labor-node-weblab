61c721d2d90fe178407f2fe7b2d045356fd945ae
Daniel Maslowski <daniel.maslowski@shop.co>
2016-08-21 16:32:23 +0200

    ~refactor CAN usage in app

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
 var conf = require('./config.js');
-var can_client = require('./can.js');
 var http = require('./http.js');
 var io = require('socket.io').listen(http.server);
 var mpd = require('mpd'),
     cmd = mpd.cmd;
+var cand = require('./cand.js'),
+    registerCANCallbacks = cand.registerCANCallbacks,
+    handleCANData = cand.handleCANData;
 
 //io.enable('browser client minification');  // send minified client
 //io.enable('browser client etag');          // apply etag caching logic based on version number
 //io.enable('browser client gzip');          // gzip the file
 //io.set('log level', 1);                    // reduce logging
-var canCallbacks = [];
-
-var registerCANCallBack = function(callback, sourceaddress, sourceport, subchannel) {
-  canCallbacks.push({
-    addr: sourceaddress,
-    port: sourceport,
-    sub: subchannel,
-    callback: callback,
-    cb: function(data) {
-      if (data.source_addr == this.addr && data.source_port == this.port) {
-        //console.log("addr = " + this.addr + " port " + this.port);
-        this.callback(data, this.sub);
-      }
-    }
-  });
-}
-
-can_client.on('packet', function(data) {
-  for (var i in canCallbacks) {
-    canCallbacks[i].cb(data);
-  }
-});
 
 for (var i in conf.rooms) {
   var room = conf.rooms[i];
@@ -38,8 +18,6 @@ for (var i in conf.rooms) {
     var device = room.devices[j];
     if (device.name == "Musik") {
       console.log("trying " + device.host + " : " + device.port);
-      var host = device.host;
-      var port = device.port;
       var client = mpd.connect({
         port: device.port,
         host: device.host,
@@ -88,43 +66,7 @@ for (var i in conf.rooms) {
   }
 }
 
-// prepare objects for fast access
-for (var i in conf.subchannels)
-{
-  var subchannel = conf.subchannels[i];
-  if (subchannel.type == "swt") {
-    registerCANCallBack(
-      function(data, sub) {
-        var byte_indx = parseInt(sub.response_bit / 8);
-        if (data.dlc > byte_indx) {
-          sub.currentvalue = data.data[byte_indx] & (1<<(sub.response_bit%8));
-          io.emit('UpdateGUI', [{'fkt': 'sw', 'dev': sub.id, 'val': sub.currentvalue}]);
-          console.log("emitting packet " + sub.id + " val " + sub.currentvalue);
-        }
-      },
-      subchannel.lapaddr,
-      subchannel.response_srcport,
-      subchannel
-    );
-  } else if (subchannel.type == "sld") {
-    registerCANCallBack(
-      function(data, sub) {
-        if (data.dlc > sub.response_byte) {
-          sub.currentvalue = data.data[sub.response_byte];
-          io.emit('UpdateGUI', [{'fkt': 'sl', 'dev': sub.id, 'val': sub.currentvalue}]);
-        }
-      },
-      subchannel.lapaddr,
-      subchannel.response_srcport,
-      subchannel
-    );
-  } else if (subchannel.type == "graph") {
-    registerCANCallBack(
-      function(data, sub) {
-        io.sockets.emit('UpdateGUI', [{'fkt': 'graph', 'dev': sub.id, 'val': data.data[sub.response_byte]}]);
-    }, subchannel.lapaddr, subchannel.response_srcport, subchannel);
-  }
-};
+registerCANCallbacks(io, conf.subchannels);
 
 io.sockets.on('connection', function (socket) {
   console.log("new connection");
@@ -177,47 +119,10 @@ io.sockets.on('connection', function (socket) {
       return;
     }
 
-    var CANPacket = can_client.CANPacket;
-    CANPacket.source_addr = 0;
-    CANPacket.destination_addr = subchannel.lapaddr;
-    CANPacket.source_port = subchannel.port;
-    CANPacket.destination_port = subchannel.port;
-    CANPacket.dlc = 0;
-    if (typeof subchannel.data0 != "undefined") {
-      CANPacket.data[0] = (data.fkt == 'lock' ? subchannel.data0 + 1 : subchannel.data0);
-      CANPacket.dlc += 1;
-    }
-    if (typeof subchannel.data1 != "undefined") {
-      CANPacket.data[1] = subchannel.data1;
-      CANPacket.dlc += 1;
-    }
-    if (typeof subchannel.data2 != "undefined") {
-      CANPacket.data[2] = subchannel.data2;
-      CANPacket.dlc += 1;
-    }
-    if (typeof subchannel.data3 != "undefined") {
-      CANPacket.data[3] = subchannel.data3;
-      CANPacket.dlc += 1;
-    }
-    if (typeof subchannel.data4 != "undefined") {
-      CANPacket.data[4] = subchannel.data4;
-      CANPacket.dlc += 1;
-    }
-    if (typeof subchannel.action != "undefined") {
-      CANPacket.data[parseInt(subchannel.action)] = parseInt(data.val);
-      if (CANPacket.dlc < parseInt(subchannel.action) + 1) {
-        CANPacket.dlc = parseInt(subchannel.action) + 1;
-      }
-    }
-    CANPacket.senddata();
-    delete CANPacket;
+    handleCANData(data, subchannel);
+
     //io.sockets.emit('message', data);
-
   });
-});
-
-can_client.on('connected', function() {
-  console.log('can connected');
 });
 
 process.on('exit', function () {
